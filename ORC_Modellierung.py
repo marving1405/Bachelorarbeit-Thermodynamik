e48ca19097353d2281d01701a9bd4f26084c9cd2
Marvin <marvin.gertenbach@stud.uni-due.de>
2022-12-01 16:54:05 +0100

    Optimierung ORC

--- ORC_Modellierung.py
+++ ORC_Modellierung.py
@@ -6,7 +6,11 @@ Created on Thu Nov  3 16:24:32 2022
 """
 
 import numpy as np
+import matplotlib.pyplot as plt
+
+
 from calculate_alpha_aw import alpha_inside_tube, alpha_outside_tube
+
 fluid = "REFPROP::PROPANE"
 # Pumpe
 """
@@ -29,7 +33,8 @@ v1 = 1 / (PropsSI('D','P',p1,'T',T1,fluid)) #m3/kg EINHEITENPROBLEM
 
 #TODO for-Schleife implementieren
 # Massenstrom
-m = 10E-3 #kg/s
+
+m = 10E-3
 w_p = (v1 * (p2-p1)) #J EINHEITENPROBLEM
 P_p = (w_p * m) / (etaP) #W???
 h2 = w_p + h1
@@ -49,27 +54,9 @@ d_i = 10E-3 #m
 d_ai = 14E-3
 d_aa = 16E-3
 
-'''
-Parameter für alpha-Berechnung
-'''
-A_quer = np.pi * d_i**2 #m2
-rho = PropsSI('D','T',T1,'P',p1,fluid)  #kg/m3
-v = m / rho
-c = v / A_quer
-viscosity = PropsSI('VISCOSITY','T',T2,'P',p2,fluid)
-
-
-re = rho * c * d_i / viscosity
-lambda_fluid = PropsSI('CONDUCTIVITY','T',T2,'P',p2,fluid)
-pr = PropsSI('PRANDTL','T',T2,'P',p2,fluid)
-alpha_i = alpha_inside_tube(re,pr,lambda_fluid, d_i)
-
-
-alpha_a = alpha_outside_tube(d_ai, d_aa, lambda_fluid)
-
 
 # Laenge des Waermeuebertragers
-l = 9 #m
+l = 10 #m
 
 #U = Wärmeübertragungskoeffizient
  #m
@@ -89,11 +76,28 @@ delta_Tb = T3 - T2
 Auslegung des Wärmeübertragers 1 (Unterkühlte Flüssigkeit zu siedender Flüssigkeit)
 
 '''
+
+'''
+Parameter für alpha-Berechnung für Wärmeübertrager 1
+'''
+A_quer = np.pi * d_i**2 #m2
+rho_1 = PropsSI('D','T',T2,'P',p2,fluid)  #kg/m3
+v_1 = m / rho_1
+c_1 = v_1 / A_quer
+viscosity_1 = PropsSI('VISCOSITY','T',T2,'P',p2,fluid)
+
+
+re_1 = rho_1 * c_1 * d_i / viscosity_1
+lambda_fluid_1 = PropsSI('CONDUCTIVITY','T',T2,'P',p2,fluid)
+pr_1 = PropsSI('PRANDTL','T',T2,'P',p2,fluid)
+alpha_i_1 = alpha_inside_tube(re_1,pr_1,lambda_fluid_1, d_i)
+alpha_a_1 = alpha_outside_tube(d_ai, d_aa, lambda_fluid_1)
+
 A_i = 2 * np.pi * d_i/2 * l/3
 A_a = 2 * np.pi * d_ai/2 * l/3
-R_konv_innen1 = 1 / A_i * alpha_i
-R_konv_aussen1 = 1 / A_a * alpha_a
-R_waermeleitung1 = np.log(d_aa/d_ai) / (2 * np.pi * l * lambda_fluid)
+R_konv_innen1 = 1 / A_i * alpha_i_1
+R_konv_aussen1 = 1 / A_a * alpha_a_1
+R_waermeleitung1 = np.log(d_aa/d_ai) / (2 * np.pi * l * lambda_fluid_1)
 
 T2_siedend = PropsSI('T','P',p2,'Q',0,fluid)
 delta_T2_1 = T2_siedend - T2
@@ -106,13 +110,13 @@ Q_zu1 = (1 / R_ges1) * delta_T2_1
 Auslegung des Wärmeübertragers 2 (siedende Flüssigkeit zu Sattdampf)
 isotherme Zustandsänderung, daher über 1.HS
 '''
-
+lambda_fluid_2 = PropsSI('CONDUCTIVITY','T',T2_siedend,'P',p2,fluid)
 alpha_i_zweiphasig = 600
 alpha_a_zweiphasig = 400
 # TODO andere alphas aus VDI Waermeatlas ok?
 R_konv_innen2 = 1 / A_i * alpha_i_zweiphasig
 R_konv_aussen2 = 1 / A_a * alpha_a_zweiphasig
-R_waermeleitung2 = np.log(d_aa/d_ai) / (2 * np.pi * l * lambda_fluid)
+R_waermeleitung2 = np.log(d_aa/d_ai) / (2 * np.pi * l * lambda_fluid_2)
 R_ges2 = R_konv_innen2 + R_konv_aussen2 + R_waermeleitung2
 h2_sattdampf = PropsSI('H','P',p2,'Q',1,fluid)
 h2_siedend = PropsSI('H','P',p2,'T',T2_siedend,fluid)
@@ -120,12 +124,28 @@ Q_zu2 = h2_sattdampf - h2_siedend
 
 '''
 Auslegung des Wärmeübertragers 3 (Sattdampf zu überhitzten Dampf)
-
+'''
+'''
+Parameter für alpha-Berechnung für Wärmeübertrager 3
 '''
 
-R_konv_innen3 = 1 / A_i * alpha_i
-R_konv_aussen3 = 1 / A_a * alpha_a
-R_waermeleitung3 = np.log(d_aa/d_ai) / (2* np.pi * l * lambda_fluid)
+
+rho_3 = PropsSI('D','T',T2_heat,'P',p2,fluid)  #kg/m3
+v_3 = m / rho_3
+c_3 = v_3 / A_quer
+viscosity_3 = PropsSI('VISCOSITY','T',T2_heat,'P',p2,fluid)
+
+
+re_3 = rho_3 * c_3 * d_i / viscosity_3
+lambda_fluid_3 = PropsSI('CONDUCTIVITY','T',T2_heat,'P',p2,fluid)
+pr_3 = PropsSI('PRANDTL','T',T2_heat,'P',p2,fluid)
+alpha_i_3 = alpha_inside_tube(re_3,pr_3,lambda_fluid_3, d_i)
+alpha_a_3 = alpha_outside_tube(d_ai, d_aa, lambda_fluid_3)
+
+
+R_konv_innen3 = 1 / A_i * alpha_i_3
+R_konv_aussen3 = 1 / A_a * alpha_a_3
+R_waermeleitung3 = np.log(d_aa/d_ai) / (2* np.pi * l * lambda_fluid_3)
 
 h3 = PropsSI('H','T',T3,'P',p2,fluid)
 T2_sattdampf = PropsSI('T','P',p2,'Q',1,fluid)
@@ -152,25 +172,52 @@ P_t = m * (h4 - h3) * etaT
 
 # Kondensator 1
 # Ist hier das alpha gleich wie beim Verdampfer? Und Einheiten der Wärme
+rho_k1 = PropsSI('D','T',T4,'P',p4,fluid)  #kg/m3
+v_k1 = m / rho_k1
+c_k1 = v_k1 / A_quer
+viscosity_k1 = PropsSI('VISCOSITY','T',T4,'P',p4,fluid)
 
-A_i = 2 * np.pi * d_i/2 * l
+
+re_k1 = rho_k1 * c_k1 * d_i / viscosity_k1
+lambda_fluid_k1 = PropsSI('CONDUCTIVITY','T',T4,'P',p4,fluid)
+pr_k1 = PropsSI('PRANDTL','T',T4,'P',p4,fluid)
+
+alpha_i_k1 = alpha_inside_tube(re_k1,pr_k1,lambda_fluid_k1, d_i)
+alpha_a_k1 = alpha_outside_tube(d_ai, d_aa, lambda_fluid_k1)
+
+A_i = 2 * np.pi * d_i/2 * l/2
 A_a = 2 * np.pi * d_ai/2 * l
-R_konv_innen = 1 / A_i * alpha_i
-R_konv_aussen = 1 / A_a * alpha_a
-R_waermeleitung = np.log(d_aa/d_ai) / (2 * np.pi * l * lambda_fluid)
+R_konv_innen = 1 / A_i * alpha_i_k1
+R_konv_aussen = 1 / A_a * alpha_a_k1
+R_waermeleitung = np.log(d_aa/d_ai) / (2 * np.pi * l * lambda_fluid_k1)
+
+
+
 
 # Kondensator 2
 # Ist hier das alpha gleich wie beim Verdampfer? Und Einheiten der Wärme
+rho_k2 = PropsSI('D','T',T4,'P',p4,fluid)  #kg/m3
+v_k2 = m / rho_k2
+c_k2 = v_k2 / A_quer
+viscosity_k2 = PropsSI('VISCOSITY','T',T4,'P',p4,fluid)
 
-A_i = 2 * np.pi * d_i/2 * l
-A_a = 2 * np.pi * d_ai/2 * l
-R_konv_innen = 1 / A_i * alpha_i
-R_konv_aussen = 1 / A_a * alpha_a
-R_waermeleitung = np.log(d_aa/d_ai) / (2 * np.pi * l * lambda_fluid)
+
+re_k2 = rho_k2 * c_k2 * d_i / viscosity_k2
+lambda_fluid_k2 = PropsSI('CONDUCTIVITY','T',T4,'P',p4,fluid)
+pr_k2 = PropsSI('PRANDTL','T',T4,'P',p4,fluid)
+
+alpha_i_k2 = alpha_inside_tube(re_k2,pr_k2,lambda_fluid_k2, d_i)
+alpha_a_k2 = alpha_outside_tube(d_ai, d_aa, lambda_fluid_k2)
+
+R_konv_innen = 1 / A_i * alpha_i_k2
+R_konv_aussen = 1 / A_a * alpha_a_k2
+R_waermeleitung = np.log(d_aa/d_ai) / (2 * np.pi * l * lambda_fluid_k2)
 
 delta_T41 = T4 - T1
 R_ges = R_konv_innen1 + R_konv_aussen1 + R_waermeleitung1
 Q_ab = (1 / R_ges) * delta_T41
+
+
 
 
 "Berechnung thermischer Wirkungsgrad"
@@ -178,3 +225,9 @@ Q_ab = (1 / R_ges) * delta_T41
 w_netto = abs(w_t + w_p)
 eta_th = w_netto / (Q_zu_ges)
 print(eta_th)
+
+#m = list(range(5E-3,40E-3,1E-3)) #kg/s
+#plt.plot(m,eta_th)
+#plt.show()
+
+
