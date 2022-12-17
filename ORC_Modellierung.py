9983122a46ce7fbce3d2f410171e6f0e487c9014
Marvin <marvin.gertenbach@stud.uni-due.de>
2022-12-17 16:02:03 +0100

    Optimierung ORC

--- ORC_Modellierung.py
+++ ORC_Modellierung.py
@@ -33,19 +33,17 @@ etaP = 0.9
 # Eintrittszustand bei 1 bar unterkühlte Flüssigkeit
 
 # Import Stoffdaten
-#import CoolProp.CoolProp as CP
-#from CoolProp.CoolProp import PropsSI
 
 h1 = PropsSI('H','T',T1,'P',p1,fluid)
 p2 = 2000000 #Pa
-v1 = 1 / (PropsSI('D','P',p1,'T',T1,fluid)) #m3/kg EINHEITENPROBLEM
+v1 = 1 / (PropsSI('D','P',p1,'T',T1,fluid)) #m3/kg
 
 
 # Massenstrom
 
-m = 10E-3
-w_p = (v1 * (p2-p1)) #J EINHEITENPROBLEM
-P_p = (w_p * m) / (etaP) #W???
+m_ORC = 10E-3
+w_p = (v1 * (p2-p1)) #J
+P_p = (w_p * m_ORC) / (etaP)
 h2 = w_p + h1
 T2 = PropsSI('T','H',h2,'P',p2,fluid)
 
@@ -67,19 +65,14 @@ d_aa = 16E-3
 # Laenge des Waermeuebertragers
 l = 10 #m
 
-#U = Wärmeübertragungskoeffizient
- #m
-#A = #Austauschfläche m2
 Te_1 = 200 + 273.15
-#Ta_1 = 100 + 273.15
-T3 = 367.456 #Mindesttemperatur ist 150°C max. 200°C
-#p2 = p3
+T3 = 367.456 #Berechnet mit LMTD-Methode
 h3 = PropsSI('H','T',T3,'P',p2,fluid)
 
 """
 Berechnung der Speicherfluideigenschaften für den Mitteltemperaturtank
 """
-m_oel = 40E-3 #kg/s
+m_oel = 40E-3 #kg/s #TODO implemtieren Verhältnis aus ORC Massenstrom und Oelmassenstrom
 cp_oel = 1.9 #kJ/kg*K
 Tmittel_H = 100 + 273.15 #K
 
@@ -90,11 +83,11 @@ Auslegung des Wärmeübertragers 1 (Unterkühlte Flüssigkeit zu siedender Flüs
 '''
 
 '''
-Parameter für alpha-Berechnung für Wärmeübertrager 1 (Rekuperator)
+Parameter für alpha-Berechnung für Wärmeübertrager 1 mit Tank
 '''
 A_quer = np.pi * (d_i/2)**2 #m2
 rho_1 = PropsSI('D','T',T2,'P',p2,fluid)  #kg/m3
-v_1 = m / rho_1
+v_1 = m_ORC / rho_1
 c_1 = v_1 / A_quer
 viscosity_1 = PropsSI('VISCOSITY','T',T2,'P',p2,fluid)
 
@@ -105,32 +98,29 @@ pr_1 = PropsSI('PRANDTL','T',T2,'P',p2,fluid)
 alpha_i_1 = alpha_inside_tube(re_1,pr_1,lambda_fluid_1, d_i)
 alpha_a_1 = alpha_outside_tube(d_ai, d_aa, lambda_fluid_1)
 
-A_i = 2 * np.pi * d_i/2 * l/3
-A_a = 2 * np.pi * d_ai/2 * l/3
-R_konv_innen1 = 1 / A_i * alpha_i_1
-R_konv_aussen1 = 1 / A_a * alpha_a_1
-R_waermeleitung1 = np.log(d_aa/d_ai) / (2 * np.pi * l * lambda_fluid_1)
+
 
 T2_siedend = PropsSI('T','P',p2,'Q',0,fluid)
 delta_T2_1 = T2_siedend - T2
-R_ges1 = R_konv_innen1 + R_konv_aussen1 + R_waermeleitung1
+
 cp_fluid_1 = PropsSI('C', 'T', T2, 'P', p2, fluid)
-Q_zu1 = m * cp_fluid_1 * delta_T2_1
+Q_zu1 = m_ORC * cp_fluid_1 * delta_T2_1
 
 Tlow_H = 60 + 273.15 #K
 Tlow_L = 30 + 273.15 #K
 l1 = Q_zu1 / (np.pi * d_i * alpha_i_1 * (Tlow_H - Tlow_L))
 
-
+A_i = 2 * np.pi * d_i/2 * l1/3
+A_a = 2 * np.pi * d_ai/2 * l1/3
+R_konv_innen1 = 1 / A_i * alpha_i_1
+R_konv_aussen1 = 1 / A_a * alpha_a_1
+R_waermeleitung1 = np.log(d_aa/d_ai) / (2 * np.pi * l1 * lambda_fluid_1)
+R_ges1 = R_konv_innen1 + R_konv_aussen1 + R_waermeleitung1
 '''
 Auslegung des Wärmeübertragers 2 (siedende Flüssigkeit zu Sattdampf)
 isotherme Zustandsänderung, daher über 1.HS
 '''
 
-
-
-
-# TODO keine Temperaturdifferenz -> inwiefern therm. Widerstände einbinden?
 lambda_fluid_2 = PropsSI('CONDUCTIVITY','T',T2_siedend,'P',p2,fluid)
 alpha_i_zweiphasig = 600
 alpha_a_zweiphasig = 400
@@ -141,14 +131,10 @@ R_waermeleitung2 = np.log(d_aa/d_ai) / (2 * np.pi * l * lambda_fluid_2)
 R_ges2 = R_konv_innen2 + R_konv_aussen2 + R_waermeleitung2
 h2_sattdampf = PropsSI('H','P',p2,'Q',1,fluid)
 h2_siedend = PropsSI('H','P',p2,'T',T2_siedend,fluid)
-Q_zu2 = m * (h2_sattdampf - h2_siedend)
+Q_zu2 = m_ORC * (h2_sattdampf - h2_siedend)
 Tmittel_L = Tmittel_H - ((Q_zu2/1000)/(m_oel * cp_oel))
 
 l2 = Q_zu2 / (np.pi * d_i * alpha_i_zweiphasig * (Tmittel_H-Tmittel_L))
-
-#Mitteltemperatur
-
-
 
 '''
 Auslegung des Wärmeübertragers 3 (Sattdampf zu überhitzten Dampf)
@@ -165,7 +151,7 @@ h3 = PropsSI('H','T',T3,'P',p2,fluid)
 T2_sattdampf = PropsSI('T','P',p2,'Q',1,fluid)
 
 rho_3 = PropsSI('D','T',T2_sattdampf,'P',p2,fluid)  #kg/m3
-v_3 = m / rho_3
+v_3 = m_ORC / rho_3
 c_3 = v_3 / A_quer
 viscosity_3 = PropsSI('VISCOSITY','T',T2_sattdampf,'P',p2,fluid)
 
@@ -185,7 +171,7 @@ R_waermeleitung3 = np.log(d_aa/d_ai) / (2* np.pi * l3 * lambda_fluid_3)
 R_ges3 = R_konv_innen3 + R_konv_aussen3 + R_waermeleitung3
 delta_T2_2 = T3 - T2_sattdampf
 cp_fluid_3 = PropsSI('C', 'T', T2_siedend, 'P', p2, fluid)
-Q_zu3 = m * (h3 - h2_sattdampf)#m * cp_fluid_3 * delta_T2_2
+Q_zu3 = m_ORC * (h3 - h2_sattdampf)
 dTA = Thoch_H - Thoch_L
 '''
 from scipy.optimize import fsolve
@@ -222,20 +208,21 @@ Q_zu_ges = Q_zu1 + Q_zu2 + Q_zu3
 
 from isentroper_Wirkungsgrad_Expander import isentroper_Wirkungsgrad
 n = 5000
-eta_Expander = isentroper_Wirkungsgrad(m,n)
+eta_Expander = isentroper_Wirkungsgrad(m_ORC, n)
 s3 = PropsSI('S','H',h3,'P',p2,fluid)
 p4 = p1 #Druckverhältnis variieren
 h4 = PropsSI('H','S',s3,'P',p4,fluid)
 T4 = PropsSI('T','P',p4,'H',h4,fluid)
 w_t = (h4 - h3) * eta_Expander
-P_t = m * (h4 - h3) * eta_Expander
-
+P_t = m_ORC * (h4 - h3) * eta_Expander
+# TODO Druckverhältnis implementieren
 
 
 # Kondensator 1
 # Ist hier das alpha gleich wie beim Verdampfer? Und Einheiten der Wärme
+# Länge der Kondensatoren berechnen
 rho_k1 = PropsSI('D','T',T4,'P',p4,fluid)  #kg/m3
-v_k1 = m / rho_k1
+v_k1 = m_ORC / rho_k1
 c_k1 = v_k1 / A_quer
 viscosity_k1 = PropsSI('VISCOSITY','T',T4,'P',p4,fluid)
 
@@ -259,7 +246,7 @@ R_waermeleitung = np.log(d_aa/d_ai) / (2 * np.pi * l * lambda_fluid_k1)
 # Kondensator 2
 # Ist hier das alpha gleich wie beim Verdampfer? Und Einheiten der Wärme
 rho_k2 = PropsSI('D','T',T4,'P',p4,fluid)  #kg/m3
-v_k2 = m / rho_k2
+v_k2 = m_ORC / rho_k2
 c_k2 = v_k2 / A_quer
 viscosity_k2 = PropsSI('VISCOSITY','T',T4,'P',p4,fluid)
 
@@ -278,7 +265,7 @@ R_waermeleitung = np.log(d_aa/d_ai) / (2 * np.pi * l * lambda_fluid_k2)
 delta_T41 = T4 - T1
 R_ges = R_konv_innen1 + R_konv_aussen1 + R_waermeleitung1
 cp_fluid_41 = PropsSI('C', 'T', T4, 'P', p1, fluid)
-Q_ab = m * cp_fluid_41 * delta_T41
+Q_ab = m_ORC * cp_fluid_41 * delta_T41
 
 
 
@@ -299,8 +286,20 @@ plt.show()
 
 
 from calculate_alpha_aw import alpha_1P_i
-#Rekuperator
-alpha_R = alpha_1P_i(p4, T4, fluid, m, d_i)
+'''
+if Rekuperation == 1:
+    # Rekuperator
+    alpha_R = alpha_1P_i(p4, T4, fluid, m_ORC, d_i)
+    # Berechnung nach lmtd-Methode Gleichung nach Temperatur mit l = 5m auflösen (mit Q_zu1)
+    T5 = 247.113  # K
+else: #Berechnung mit WÜ1 und Tanks
+    #Programmierung WÜ
+'''
+
+
+#TODO Kondensatoren mit Rekuperator in if-Schleife implementieren (und WÜ1)
+
+
 
 
 
