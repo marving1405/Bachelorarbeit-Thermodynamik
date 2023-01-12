434a18b6de7036d1ce9992496833d572f6db1ac9
marvi <marvin.gertenbach@stud.uni-due.de>
2023-01-12 18:20:47 +0100

    Implementierung T3 Solver

--- ORC_Modellierung.py
+++ ORC_Modellierung.py
@@ -47,9 +47,9 @@ d_aa = 16E-3
 from Test_fsolve import solveT3
 
 #T3 = 1
-solveT3() #TODO Argumente übergeben
+
 #T3 = 367.456 #K Berechnet mit LMTD-Methode -> Solver-Funktion
-h3 = PropsSI('H','T',T3,'P',p2,fluid)
+
 
 #TODO Implementieren Massenstromverhältnis
 #for x in np.arange(0.1,10,0.1):
@@ -92,8 +92,10 @@ alpha_i_1 = alpha_1P_i(p2,T2,fluid,m_ORC,d_i)
 
 
 Tlow_H = 60 + 273.15 #K
-Tlow_L = 30 + 273.15 #K
-l1 = Q_zu1 / (np.pi * d_i * alpha_i_1 * (Tlow_H - Tlow_L))
+Tlow_L = 20 + 273.15 #K
+dTA_1 = T2_siedend - T2
+dTB_1 = Tlow_H - Tlow_L
+l1 = Q_zu1 / (np.pi * d_i * alpha_i_1 * ((dTA_1) - (dTB_1)/np.log(dTA_1/dTB_1)))
 
 A_i = 2 * np.pi * d_i/2 * l1/3
 A_a = 2 * np.pi * d_ai/2 * l1/3
@@ -102,7 +104,7 @@ R_konv_aussen1 = 1 / A_a * alpha_a_1
 R_waermeleitung1 = np.log(d_aa/d_ai) / (2 * np.pi * l1 * lambda_fluid_1)
 R_ges1 = R_konv_innen1 + R_konv_aussen1 + R_waermeleitung1
 
-
+print(l1)
 '''
 Auslegung des Wärmeübertragers 2 (siedende Flüssigkeit zu Sattdampf)
 isotherme Zustandsänderung, daher über 1.HS
@@ -135,21 +137,22 @@ Auslegung des Wärmeübertragers 3 (Sattdampf zu überhitzten Dampf)
 
 
 Thoch_H = 180 + 273.15 #K
-Thoch_L = 110 + 273.15 #K
+Thoch_L = 120 + 273.15 #K
 l3 = 5 #m
-
-h3 = PropsSI('H','T',T3,'P',p2,fluid)
+Q_zu3 = m_oel * cp_oel * (Thoch_H - Thoch_L) * 1000
+#h3 = PropsSI('H','T',T3,'P',p2,fluid)
 T2_sattdampf = PropsSI('T','P',p2,'Q',1,fluid)
 
 rho_3 = PropsSI('D','T',T2_sattdampf,'Q',1,fluid)  #kg/m3
 v_3 = m_ORC / rho_3
 c_3 = v_3 / A_quer
+
 viscosity_3 = PropsSI('VISCOSITY','T',T2_sattdampf,'Q',1,fluid)
 
 re_3 = rho_3 * c_3 * d_i / viscosity_3
 lambda_fluid_3 = PropsSI('CONDUCTIVITY','T',T2_sattdampf,'Q',1,fluid)
 pr_3 = PropsSI('PRANDTL','T',T2_sattdampf,'Q',1,fluid)
-alpha_i_3 = alpha_inside_tube(re_3,pr_3,lambda_fluid_3, d_i)
+alpha_i_3 = alpha_inside_tube(re_3,pr_3,lambda_fluid_3, d_i) #alpha_1P_i(p2, T2_sattdampf, fluid, m_ORC, d_i)
 alpha_a_3 = alpha_outside_tube(d_ai, d_aa, lambda_fluid_3)
 
 A_i_3 = 2 * np.pi * d_i/2 * l3/3
@@ -160,23 +163,26 @@ R_waermeleitung3 = np.log(d_aa/d_ai) / (2 * np.pi * l3 * lambda_fluid_3)
 
 
 R_ges3 = R_konv_innen3 + R_konv_aussen3 + R_waermeleitung3
-delta_T2_2 = T3 - T2_sattdampf
+#delta_T2_2 = T3 - T2_sattdampf
 cp_fluid_3 = PropsSI('C', 'T', T2_siedend, 'Q', 0, fluid)
-Q_zu3 = m_ORC * (h3 - h2_sattdampf)
+#Q_zu3 = m_ORC * (h3 - h2_sattdampf)
 dTA = Thoch_H - Thoch_L
 
+from scipy.optimize import fsolve
+T3 = fsolve(solveT3, 350., args = (Q_zu3, d_i, l3, alpha_i_3, T2_sattdampf, dTA))
 
+h3 = PropsSI('H','T',T3,'P',p2,fluid)
 Q_zu_ges = Q_zu1 + Q_zu2 + Q_zu3
-
+print(T3,h2)
 
 # Turbine
 
 from isentroper_Wirkungsgrad_Expander import isentroper_Wirkungsgrad
 n = 5000
 eta_Expander = isentroper_Wirkungsgrad(m_ORC, n)
-s3 = PropsSI('S','H',h3,'P',p2,fluid)
+s3 = PropsSI('S','P',p2,'H',h3,fluid)
 p4 = p1 #Druckverhältnis variieren
-h4 = PropsSI('H','S',s3,'P',p4,fluid)
+h4 = PropsSI('H','S',s3,'P',p1,fluid)
 T4 = PropsSI('T','P',p4,'H',h4,fluid)
 w_t = (h4 - h3) * eta_Expander
 P_t = m_ORC * (h4 - h3) * eta_Expander
@@ -184,7 +190,7 @@ P_t = m_ORC * (h4 - h3) * eta_Expander
 verhaeltnis = p2/p4
 
 
-# Kondensator 1
+# Kondensator 1, ÜD -> SF
 
 #TODO Länge der Kondensatoren mit LMTD berechnen
 l_k1 = 5 #m Annahme
@@ -201,18 +207,20 @@ pr_k1 = PropsSI('PRANDTL','T',T4,'P',p4,fluid)
 alpha_i_k1 = alpha_inside_tube(re_k1,pr_k1,lambda_fluid_k1, d_i)
 alpha_a_k1 = alpha_outside_tube(d_ai, d_aa, lambda_fluid_k1)
 
-h4_siedend = PropsSI('H','P',p4,'Q',1,fluid)
+h4_siedend = PropsSI('H','P',p4,'Q',0,fluid)
 Q_ab1 = m_ORC * (h4 - h4_siedend)
 
-A_i = 2 * np.pi * d_i/2 * l_k1/2
-A_a = 2 * np.pi * d_ai/2 * l_k1
+A_i_k1 = 2 * np.pi * d_i/2 * l_k1/2
+A_a_k1 = 2 * np.pi * d_ai/2 * l_k1
 R_konv_innen = 1 / A_i * alpha_i_k1
 R_konv_aussen = 1 / A_a * alpha_a_k1
 R_waermeleitung = np.log(d_aa/d_ai) / (2 * np.pi * l_k1 * lambda_fluid_k1)
 
+dTA_k1 = T4 - T4_siedend
+dTB_k1 = -20 #Kühlwasser etc.
+l_k1 = Q_ab1 / (np.pi * d_i * alpha_i_k1 * ((dTA_k1) - (dTB_k1)/np.log(dTA_k1/dTB_k1)))
 
-
-# Kondensator 2
+# Kondensator 2 SF -> UK
 l_k2 = 5 #m
 rho_k2 = PropsSI('D','T',T4,'P',p4,fluid)  #kg/m3
 v_k2 = m_ORC / rho_k2
@@ -233,7 +241,9 @@ R_konv_innen = 1 / A_i * alpha_i_k2
 R_konv_aussen = 1 / A_a * alpha_a_k2
 R_waermeleitung = np.log(d_aa/d_ai) / (2 * np.pi * l_k2 * lambda_fluid_k2)
 
-
+dTA_k2 = T4_siedend - T1
+dTB_k2 = -30 #Kühlwasser etc.
+l_k2 = Q_ab2 / (np.pi * d_i * alpha_i_k2 * ((dTA_k2) - (dTB_k2)/np.log(dTA_k2/dTB_k2)))
 
 
 "Berechnung thermischer Wirkungsgrad"
--- Test_fsolve.py
+++ Test_fsolve.py
@@ -1,21 +1,15 @@
 from scipy.optimize import fsolve
 import numpy as np
-import math
+'''''
 Q_zu3 = 5320 #W
 d_i = 0.01
 l3 = 5
-alpha_i_3 = 654
-
+alpha_i_3 = 727
 dTA = 70
-#dTB = T3 - T2_sattdampf
 T2_sattdampf = 330
-
-def solveT3(T3):
+'''''
+def solveT3(T3, Q_zu3, d_i, l3, alpha_i_3, T2_sattdampf, dTA):
 
     return [Q_zu3 / (np.pi * d_i * l3 * alpha_i_3) - (dTA - (T3[0] - T2_sattdampf))/(np.log(dTA/(T3[0] - T2_sattdampf)))]
-
-T3 = (fsolve(solveT3, 500.))
-
-print(T3)
 
 # Q_zu3 / (np.pi * d_i * l3 * alpha_i_3) - (dTA - (T3[0] - T2_sattdampf))/(np.log(dTA/(T3[0] - T2_sattdampf))) = 0
