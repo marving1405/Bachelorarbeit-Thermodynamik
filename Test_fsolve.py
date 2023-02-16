e54957f3206fd30b5f34b03f80df857afdb651c5
marving1405 <marving1405@gmail.com>
2023-02-16 21:07:11 +0100

    Berechnung Tlow für die Tanks mit vorgegebener Wü Länge

--- Test_fsolve.py
+++ Test_fsolve.py
@@ -3,7 +3,7 @@ import numpy as np
 
 def solveT3(T3, Q_zu3, R_ges3, Thoch_H, dTA_3):
 
-    return [-Q_zu3 * R_ges3 - ((dTA_3 - (Thoch_H - T3[0]))/(np.log(dTA_3)/(Thoch_H - T3[0])))]
+    return [(Q_zu3 * R_ges3) - ((dTA_3 - (Thoch_H - T3[0]))/(np.log(dTA_3/(Thoch_H - T3[0]))))]
 
 # Q_zu3 / (np.pi * d_i * l3 * alpha_i_3) - (dTA - (T3[0] - T2_sattdampf))/(np.log(dTA/(T3[0] - T2_sattdampf))) = 0
 
@@ -11,6 +11,6 @@ def solveT2_Rekuperator(T2_Rekuperator, Q_abR, d_i, lR, alpha_R, T2, dTAR):
 
     return [Q_abR / (np.pi * d_i * lR * alpha_R) - (dTAR - (T2_Rekuperator[0] - T2))/(np.log(dTAR/(T2_Rekuperator[0] - T2)))]
 
-def solveT(T ,Q_zu, R_ges, T2, dTB):
+def solveT(Tlow_L ,Q_zu1, R_ges1, T2, dTB_1):
 
-    return [(1/(Q_zu * R_ges)) * (((T[0] - T2) - dTB) / np.log((T[0] - T2) / dTB)) -1]
+    return [(Q_zu1 * R_ges1) - ((Tlow_L[0] - T2) - dTB_1) / (np.log((Tlow_L[0] - T2) / dTB_1))]
